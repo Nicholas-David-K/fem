62c828bd85eb99d94f262ce5c2bc28078452023b
Nicholas David <onedavid.nicholas@gmail.com>
2022-12-25 19:20:53 +0300

    cloudinary

--- fem/settings.py
+++ fem/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'fem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'WV2MRcvypQlfDwgUEt9u',
-#         'HOST': 'containers-us-west-155.railway.app',
-#         'PORT': '7425'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WV2MRcvypQlfDwgUEt9u',
+        'HOST': 'containers-us-west-155.railway.app',
+        'PORT': '7425'
+    }
+}
 
 
 # Password validation
